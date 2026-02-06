b72f23cf4f82ed5b2f49369d2ed23e6046a66187
KennethBaoLe <kennethble@gmail.com>
2026-02-06 16:55:24 -0500

    Gemini working

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,10 +1,13 @@
 import { streamText, UIMessage, convertToModelMessages } from 'ai';
+import { google } from '@ai-sdk/google';
+
+const model = google('gemini-2.5-flash');
 
 export async function POST(req: Request) {
     const { messages }: { messages: UIMessage[] } = await req.json();
 
     const result = streamText({
-        model: "anthropic/claude-sonnet-4.5",
+        model,
         messages: await convertToModelMessages(messages),
     });
 
